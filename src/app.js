1434ac85cf256d110a9e95e2b25dddcb550e0e86
PaulinaGJ <p.grynjablonska@gmail.com>
2022-08-30 22:06:10 +0100

    Updated current date and time

--- src/app.js
+++ src/app.js
@@ -9,9 +9,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "9deff0d49319322e8b7031db0c883dd7";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+
+//Date
+let currentDate = new Date();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[currentDate.getDay()];
+let hour = currentDate.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${day}, ${hour}:${minutes}`;
