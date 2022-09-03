0aaa6b9cdc9375eef77654f94f35a3336ce8c3c2
PaulinaGJ <p.grynjablonska@gmail.com>
2022-09-03 21:17:07 +0100

    Connected API to forecast

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+function getForecast(coordinates) {
+  let apiKey = "9deff0d49319322e8b7031db0c883dd7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -16,6 +22,8 @@ function displayTemperature(response) {
   );
   mainIconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -44,7 +52,7 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
@@ -88,4 +96,3 @@ let dateElement = document.querySelector("#date");
 dateElement.innerHTML = `${day}, ${hour}:${minutes}`;
 
 search("Dublin");
-displayForecast();
